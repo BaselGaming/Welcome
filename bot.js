e03f466d0928039ee557a7f0080d93de22dbf9ea
BaselGaming <45512285+BaselGaming@users.noreply.github.com>
2018-12-01 15:32:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client(); 
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("ا518174539440717835");
+const mohamed= member.guild.channels.get("518174539440717835");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**-# Welcomme , To Versus Server :heart:**`), 4000)        
